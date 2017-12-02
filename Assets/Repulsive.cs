c37d2234f85a93aed615b4be2c8ec974d65596a1
William Terry <Tezza48is@aol.com>
2017-12-02 02:43:34 +0000

    Adding UnityProject

---
+++ Assets/Repulsive.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Repulsive : MonoBehaviour
+{
+
+    public static List<Repulsive> sSpawnedRepulsives;
+
+    public float repellRange = 1.0f;
+    public float repellforce = 1.0f;
+
+    [Header("ComponentRefs")]
+    public Rigidbody2D mRigid;
+
+    private void Awake()
+    {
+        if (sSpawnedRepulsives == null)
+        {
+            sSpawnedRepulsives = new List<Repulsive>();
+        }
+    }
+    private void OnEnable()
+    {
+        sSpawnedRepulsives.Add(this);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 force = new Vector2();
+        foreach (Repulsive item in sSpawnedRepulsives)
+        {
+            if (item != this)
+            {
+                if (Vector2.Distance(transform.position, item.transform.position) < repellRange)
+                {
+                    force += (Vector2)(transform.position - item.transform.position);
+
+                }
+            }
+        }
+        mRigid.AddForce(force * repellforce, ForceMode2D.Force);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, repellRange);
+    }
+
+}
