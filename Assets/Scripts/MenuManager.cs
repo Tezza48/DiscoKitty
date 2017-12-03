30e66d399ed1cc3884c065deea733f30cb719212
William Terry <Tezza48is@aol.com>
2017-12-03 16:40:09 +0000

    Menu Scene Added

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public string firstLevelName = "Level_C1Circle1";
+
+    public void BtnPlayPressed()
+    {
+        SceneManager.LoadScene(firstLevelName);
+    }
+    
+}
