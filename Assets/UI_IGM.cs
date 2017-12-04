3c970e35401588300d85f93e7c982c4f3dbb66e2
William Terry <Tezza48is@aol.com>
2017-12-04 00:01:24 +0000

    IGM added, c9circle1 has smaller bounds

---
+++ Assets/UI_IGM.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UI_IGM : MonoBehaviour
+{
+
+    bool isOpen = false;
+    public RectTransform mMenu;
+    public float openSpeed = 1.0f;
+
+    public Vector2 open, closed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mMenu.pivot = Vector2.Lerp(mMenu.pivot, isOpen ? open : closed, Time.deltaTime * openSpeed);
+    }
+
+    public void ToggleMenu()
+    {
+        isOpen = !isOpen;
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
