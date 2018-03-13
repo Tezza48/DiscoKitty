4eb34346b9d13087a8e83af896e7d4d49055a1d0
William Terry <Tezza48is@aol.com>
2018-03-13 22:42:48 +0000

    Circle zone is editable with a slider in the editor

---
+++ Assets/ZoneSetup.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ZoneSetup: MonoBehaviour
+{
+    [Range (1.0f, 10.0f)]
+    public float size = 1.0f;
+
+    public SpriteRenderer artwork;
+    public CircleCollider2D collider;
+
+    public float[] sizes;
+    public Vector2[] scaleRanges;
+    public Sprite[] sprites;
+
+    private void Start()
+    {
+        if (EditorApplication.isPlaying)
+        {
+            enabled = false;
+        }
+        else
+        {
+            enabled = true;
+        }
+    }
+
+    // Use this for initialization
+    void SetupZone()
+    {
+        int selectedSize = sizes.Length - 1;
+
+        for (int i = 0; i < sizes.Length; i++)
+        {
+            if (size < sizes[i])
+            {
+                selectedSize = i;
+                break;
+            }
+        }
+
+        // Set the artwork
+        artwork.sprite = sprites[selectedSize];
+        // Set the scale
+
+        float scale = Mathf.Lerp(scaleRanges[selectedSize].x, scaleRanges[selectedSize].y, size / 10.0f);
+
+        artwork.transform.localScale = new Vector3(scale, scale, scale);
+
+        collider.radius = size / 2.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetupZone();
+    }
+}
