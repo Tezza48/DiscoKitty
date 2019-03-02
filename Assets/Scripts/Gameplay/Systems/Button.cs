9c5ed3d8d227b7a3b79152f2c40af97f1d994974
William Terry <Tezza48is@aol.com>
2019-03-02 19:15:51 +0000

    door prototype that opens and closes

---
+++ Assets/Scripts/Gameplay/Systems/Button.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+	private float isDown;
+	public Door door;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void OnTriggerEnter2D(Collider2D collider) {
+		Debug.Log(collider.name);
+		door.Open();
+	}
+
+	public void OnTriggerExit2D(Collider2D collider) {
+		door.Close();
+	}
+}
---
+++ Assets/Scripts/Gameplay/Systems/Door.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+	private bool isOpen = false;
+	public Transform doorHolder;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isOpen) {
+			doorHolder.localScale = Vector2.Lerp(doorHolder.localScale, new Vector2(0.1f, 1.0f), Time.deltaTime);
+		} else {
+			doorHolder.localScale = Vector2.Lerp(doorHolder.localScale, new Vector2(1.0f, 1.0f), Time.deltaTime);
+
+		}
+	}
+
+	public void Open() {
+		isOpen = true;
+	}
+
+	public void Close() {
+		isOpen = false;
+	}
+}
