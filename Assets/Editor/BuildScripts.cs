8ccf1b437c3c9047d2525ce1898151c95c224caf
William Terry <Tezza48is@gmail.com>
2021-02-06 18:50:33 +0000

    Build scripts to build and deploy.

---
+++ Assets/Editor/BuildScripts.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+
+public class BuildScripts
+{
+    [MenuItem("Build/WebGL")]
+    public static void BuildWebGL()
+    {
+        var options = new BuildPlayerOptions
+        {
+            locationPathName = "Build/",
+            scenes = new string[]
+            {
+                "Assets/Scenes/Menu.unity",
+                "Assets/Scenes/GameScene.unity"
+            },
+            targetGroup = BuildTargetGroup.WebGL,
+            target = BuildTarget.WebGL,
+            options = BuildOptions.None
+        };
+        BuildPipeline.BuildPlayer(options);
+    }
+}
