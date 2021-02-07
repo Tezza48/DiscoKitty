a19e8e515b80579f2be92394af54b1321a649b6e
William Terry <Tezza48is@gmail.com>
2021-02-07 13:51:15 +0000

    Settings dialog on main screen.

---
+++ Assets/SettingsDialog.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsDialog : MonoBehaviour
+{
+
+    public Toggle PointerOffsetToggle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PointerOffsetToggle.SetIsOnWithoutNotify(SettingsHelper.PointerDragOffsetEnabled);
+        PointerOffsetToggle.onValueChanged.AddListener(OnPointerDragOffsetEnableChanged);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerDragOffsetEnableChanged(bool value)
+    {
+        SettingsHelper.PointerDragOffsetEnabled = value;
+    }
+}
